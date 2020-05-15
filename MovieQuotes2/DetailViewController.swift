9ea039a6e1a519ae973c6f294b1f13e978c6ef05
belegz <zhangtiantian1999@gmail.com>
2020-05-14 21:37:33 -0400

    Initial Commit

---
+++ MovieQuotes2/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  MovieQuotes2
+//
+//  Created by Tinuviel on 2020/5/14.
+//  Copyright © 2020 Tinuviel. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
