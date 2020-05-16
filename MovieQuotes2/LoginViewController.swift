f0ec0899fc3dde5e6faf96d0542bb1772ed8d0dd
belegz <zhangtiantian1999@gmail.com>
2020-05-16 15:09:07 -0400

    UI for email password

---
+++ MovieQuotes2/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  MovieQuotes2
+//
+//  Created by Tinuviel on 2020/5/16.
+//  Copyright © 2020 Tinuviel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController{
+    let ShowListSegueIdentifier = "ShowListSegue"
+    
+    
+    @IBOutlet weak var emailTestField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func pressedSignInNewUser(_ sender: Any) {
+    }
+    
+    @IBAction func pressedLoginExistingUser(_ sender: Any) {
+    }
+}
