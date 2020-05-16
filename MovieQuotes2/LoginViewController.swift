2fe934b4f0a80e390cc2eb821f9ca50f44bacf95
belegz <zhangtiantian1999@gmail.com>
2020-05-16 15:24:08 -0400

    sign in and log in email password works!

--- MovieQuotes2/LoginViewController.swift
+++ MovieQuotes2/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class LoginViewController: UIViewController{
     let ShowListSegueIdentifier = "ShowListSegue"
@@ -15,10 +17,41 @@ class LoginViewController: UIViewController{
     @IBOutlet weak var emailTestField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTestField.placeholder = "Email"
+        passwordTextField.placeholder = "Password"
+    }
     
     @IBAction func pressedSignInNewUser(_ sender: Any) {
+        let email = emailTestField.text!
+        let password = passwordTextField.text!
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error creating a new user for Email/Password \(error)")
+                return
+            }
+            
+            print("It worked! A new user is created and now signed in!")
+            print("Email: \(authResult?.user.email)  UID: \(authResult!.user.uid)")
+            self.performSegue(withIdentifier: self.ShowListSegueIdentifier, sender: self)
+        }
     }
     
     @IBAction func pressedLoginExistingUser(_ sender: Any) {
+        let email = emailTestField.text!
+        let password = passwordTextField.text!
+        
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print("Error logging in an existing user for Email/Password \(error)")
+                return
+            }
+            
+            print("It worked! An existing user is now signed in!")
+            print("Email: \(authResult?.user.email)  UID: \(authResult!.user.uid)")
+            self.performSegue(withIdentifier: self.ShowListSegueIdentifier, sender: self)
+        }
     }
 }
